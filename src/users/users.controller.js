5700455e149dae3b7dfa3e426146bdac8f25ae13
Alen Snajder <alen.snajder@gmail.com>
2019-04-29 23:51:03 +0200

    Add get all users endpoint

--- src/users/users.controller.js
+++ src/users/users.controller.js
@@ -1,5 +1,14 @@
 import User from './users.model';
 
+export async function get(req, res, next) {
+  try {
+    const users = await User.fetchAll();
+    return res.status(200).json(users);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}
+
 export async function create(req, res, next) {
   try {
     const user = await User.forge({ email: req.body.email }).fetch();
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -3,6 +3,7 @@ import * as UsersController from './users.controller';
 
 const router = express.Router();
 
+router.get('/', UsersController.get);
 router.post('/', UsersController.create);
 
 export default router;
