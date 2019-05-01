968f5a08ff6ad4ee1707696161e64c48c1b75409
Alen Snajder <alen.snajder@gmail.com>
2019-05-02 00:41:38 +0200

    Add get all gates endpoint

--- src/gates/gates.controller.js
+++ src/gates/gates.controller.js
@@ -1,5 +1,14 @@
 import Gate from './gates.model';
 
+export async function get(req, res, next) {
+  try {
+    const gates = await Gate.fetchAll();
+    return res.status(200).json(gates);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}
+
 export async function create(req, res, next) {
   try {
     await Gate.forge(req.body).save();
--- src/gates/gates.router.js
+++ src/gates/gates.router.js
@@ -3,6 +3,7 @@ import * as GatesController from './gates.controller';
 
 const router = express.Router();
 
+router.get('/', GatesController.get);
 router.post('/', GatesController.create);
 
 export default router;
