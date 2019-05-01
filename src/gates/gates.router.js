e22abcc82781f6fffa6fb7cfaa6bb068f3469e2e
Alen Snajder <alen.snajder@gmail.com>
2019-05-02 00:49:04 +0200

    Add authentication for gate routes

--- src/gates/gates.router.js
+++ src/gates/gates.router.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import passport from 'passport';
 import * as GatesController from './gates.controller';
 
 const router = express.Router();
 
-router.get('/', GatesController.get);
-router.get('/:id', GatesController.getById);
-router.post('/', GatesController.create);
-router.put('/:id', GatesController.update);
-router.delete('/:id', GatesController.remove);
+router.get('/', passport.authenticate('jwt', { session: false }), GatesController.get);
+router.get('/:id', passport.authenticate('jwt', { session: false }), GatesController.getById);
+router.post('/', passport.authenticate('jwt', { session: false }), GatesController.create);
+router.put('/:id', passport.authenticate('jwt', { session: false }), GatesController.update);
+router.delete('/:id', passport.authenticate('jwt', { session: false }), GatesController.remove);
 
 export default router;
