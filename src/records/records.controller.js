fb2c45ce528d993187099c2d49ce4fba0d74c8d6
Alen Snajder <alen.snajder@gmail.com>
2019-05-02 01:11:34 +0200

    Add get all records endpoint

--- src/records/records.controller.js
+++ src/records/records.controller.js
@@ -1,5 +1,14 @@
 import Record from './records.model';
 
+export async function get(req, res, next) {
+  try {
+    const records = await Record.fetchAll({ withRelated: ['user', 'gate'] });
+    return res.status(200).json(records);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}
+
 export async function create(req, res, next) {
   try {
     await Record.forge(req.body).save();
--- src/records/records.router.js
+++ src/records/records.router.js
@@ -3,6 +3,7 @@ import * as RecordsController from './records.controller';
 
 const router = express.Router();
 
+router.get('/', RecordsController.get);
 router.post('/', RecordsController.create);
 
 export default router;
