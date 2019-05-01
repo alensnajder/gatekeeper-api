47d853b6120ebb1ad2dbdea2695ed4c909cf2e29
Alen Snajder <alen.snajder@gmail.com>
2019-05-02 00:46:45 +0200

    Add delete gate endpoint

--- src/gates/gates.controller.js
+++ src/gates/gates.controller.js
@@ -46,3 +46,18 @@ export async function update(req, res, next) {
     return res.status(500).json(err);
   }
 }
+
+export async function remove(req, res, next) {
+  try {
+    const gate = await Gate.forge({ id: req.params.id }).fetch();
+
+    if (!gate) {
+      return res.status(404).json('Not found');
+    }
+
+    await gate.destroy();
+    return res.status(200).send();
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}
--- src/gates/gates.router.js
+++ src/gates/gates.router.js
@@ -7,5 +7,6 @@ router.get('/', GatesController.get);
 router.get('/:id', GatesController.getById);
 router.post('/', GatesController.create);
 router.put('/:id', GatesController.update);
+router.delete('/:id', GatesController.remove);
 
 export default router;
