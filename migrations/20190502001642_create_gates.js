db0bbd27c09cfd01aac1b6121ec2d09056dcd186
Alen Snajder <alen.snajder@gmail.com>
2019-06-12 19:42:26 +0200

    Add duration field to gates

--- migrations/20190502001642_create_gates.js
+++ migrations/20190502001642_create_gates.js
@@ -1,9 +1,9 @@
-
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('gates', function(table) {
     table.increments('id').unsigned().primary();
     table.string('name').notNull();
     table.integer('gpio_pin').notNull();
+    table.integer('duration').notNull();
     table.dateTime('created_at').notNull();
     table.dateTime('updated_at').nullable();
   });
