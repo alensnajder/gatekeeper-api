3fa53238b54a485d9c0a5149f8c34534e1b928ea
Alen Snajder <alen.snajder@gmail.com>
2019-06-12 19:42:48 +0200

    Add validator for gate requests

--- src/gates/gates.router.js
+++ src/gates/gates.router.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import passport from 'passport';
 import * as GatesController from './gates.controller';
+import { createGateValidator, updateGateValidator } from './gates.validator';
 
 const router = express.Router();
 
 router.get('/', passport.authenticate('jwt', { session: false }), GatesController.get);
 router.get('/:id', passport.authenticate('jwt', { session: false }), GatesController.getById);
-router.post('/', passport.authenticate('jwt', { session: false }), GatesController.create);
-router.put('/:id', passport.authenticate('jwt', { session: false }), GatesController.update);
+router.post('/', passport.authenticate('jwt', { session: false }), createGateValidator, GatesController.create);
+router.put('/:id', passport.authenticate('jwt', { session: false }), updateGateValidator, GatesController.update);
 router.delete('/:id', passport.authenticate('jwt', { session: false }), GatesController.remove);
 
 export default router;
