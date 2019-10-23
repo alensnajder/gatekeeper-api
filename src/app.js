719857e9cceb40e999dde473649386082cc4bcf8
Alen Snajder <alen.snajder@gmail.com>
2019-10-23 23:21:03 +0200

    Add morgan logger

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import passport from 'passport';
+import morgan from 'morgan';
 import authRouter from './auth/auth.router';
 import usersRouter from './users/users.router';
 import gatesRouter from './gates/gates.router';
@@ -10,7 +11,10 @@ import passportConfig from './config/passport';
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use(passport.initialize());
+
+console.log(app.get('env'));
 
 app.use('/v1/auth', authRouter);
 app.use('/v1/users', usersRouter);
